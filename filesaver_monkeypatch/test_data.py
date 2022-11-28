f176017f530242c18426ba10f7c95839b85bdeee
Michal Dobrzycki <dobrzycki@gmail.com>
2022-11-28 22:00:54 +0100

    Fixed monkeypatching

---
+++ filesaver_monkeypatch/test_data.py
@@ -0,0 +1,14 @@
+from unittest.mock import patch, Mock
+from dataproducer import ParquetArchiver
+
+
+@patch('dataproducer.FileProducer')
+def test_file_save_with_mock(test_producer_class):
+    stub_producer = Mock()
+    stub_producer.get_file_size.return_value = 10_000_001
+    test_producer_class.return_value = stub_producer
+
+    archiver = ParquetArchiver()
+    archiver.save_or_cache()
+
+    assert archiver.is_file_saved
